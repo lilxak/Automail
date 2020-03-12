d3b7b1d44e09222b1819cbf7a13b4e662e430d17
yasserbelhimer <belhimer.yasser@etu.usthb.dz>
2020-03-12 19:46:16 +0100

    Add files via upload
    separation of send () function from the main () function

--- app.py
+++ app.py
@@ -1,30 +1,9 @@
 import eel,re
-from sheet import sheet_to_dict
-from auth import autho
-from create_email import create_mail
-from send_email import send_mail
-from googleapiclient.discovery import build
-
+from send import send
 
 @eel.expose # Expose this function to Javascript
 def sendMailPy(myLink,mySybject,myContent):
-    service = build('gmail', 'v1', credentials=autho())
-    # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
-    dic = sheet_to_dict(myLink)
-    emails = ''
-    i = 0
-    if dic['Email'] != None:
-        for email in dic['Email'] :
-            if(email!=None): 
-                if(i==0):
-                    emails+=email
-                    i = 1
-                else:
-                    emails += ',' + email
-        if(i==1):
-            message = create_mail('',emails,mySybject,myContent)
-            send_mail(service,'me',message)
-    return 'done'
+    return send(myLink,mySybject,myContent)
+
 eel.init('web')
 eel.start('index.html', size=(700,700))

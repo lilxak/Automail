a2417d3ced2333857ce999ba686b4bd4e3b48afb
yasserbelhimer <belhimer.yasser@etu.usthb.dz>
2020-03-14 20:43:47 +0100

    restructuring the  project's files

---
+++ automail-1.3/automail-1.3.py
@@ -0,0 +1,9 @@
+import eel,re
+from send import send
+
+@eel.expose # Expose this function to Javascript
+def sendMailPy(myLink,mySybject,myContent):
+    return send(myLink,mySybject,myContent)
+
+eel.init('web')
+eel.start('index.html', size=(700,700))
---
+++ automail-1.3/send.py
@@ -0,0 +1,26 @@
+from sheet import sheet_to_dict
+from auth import autho
+from create_email import create_mail
+from send_email import send_mail
+from googleapiclient.discovery import build
+
+def send(myLink,mySybject,myContent):
+    service = build('gmail', 'v1', credentials=autho())
+    # Call the Gmail API
+    results = service.users().labels().list(userId='me').execute()
+    dic = sheet_to_dict(myLink)
+    emails = ''
+    i = 0
+    if dic['Email'] != None:
+        for email in dic['Email'] :
+            if(email!=None): 
+                if(i==0):
+                    emails+=email
+                    i = 1
+                else:
+                    emails += ',' + email
+        if(i==1):
+            message = create_mail('',emails,mySybject,myContent)
+            send_mail(service,'me',message)
+            return 1
+    return 0

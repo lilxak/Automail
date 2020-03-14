0c5dc71bd8fad75c351f09c7832e3cca4c51a472
yasserbelhimer <belhimer.yasser@etu.usthb.dz>
2020-03-14 22:30:34 +0100

    Remove duplicated directory

--- app.py
+++
@@ -1,9 +0,0 @@
-import eel,re
-from send import send
-
-@eel.expose # Expose this function to Javascript
-def sendMailPy(myLink,mySybject,myContent):
-    return send(myLink,mySybject,myContent)
-
-eel.init('web')
-eel.start('index.html', size=(700,700))

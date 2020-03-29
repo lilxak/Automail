52e6c5385d10b900b68f47522be4134bf7792298
yasserbelhimer <belhimer.yasser@etu.usthb.dz>
2020-03-29 19:22:42 +0100

    add the browse file to get the link of the sheet

--- automail-1.3/automail-1.3.py
+++ automail-1.3/automail-1.3.py
@@ -1,9 +1,24 @@
-import eel
+import eel,re
 from send import send
+import fileupload
+from IPython.core.display import display
+
+from tkinter import filedialog
+from tkinter import *
+
+  
 
 @eel.expose # Expose this function to Javascript
 def sendMailPy(myLink,mySybject,myContent):
     return send(myLink,mySybject,myContent)
 
+@eel.expose
+def fileDialog():
+# This is the uploader widget
+    root = Tk()
+    name =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("xlsx files","*.xlsx"),("all files","*.*")))
+    #name = root.filename
+    root.destroy()
+    return name
 eel.init('web')
 eel.start('index.html', size=(800,700))
--- automail-1.3/send.py
+++ automail-1.3/send.py
@@ -9,11 +9,20 @@ def send(myLink,mySybject,myContent):
     service = build('gmail', 'v1', credentials=autho())
     # Call the Gmail API
     results = service.users().labels().list(userId='me').execute()
-    dic = sheet_to_dict(myLink)gi
+    dic = sheet_to_dict(myLink)
+    emails = ''
+    i = 0
     if dic['Email'] != None:
         for email in dic['Email'] :
             if(email!=None): 
-                message = create_message_with_attachment('',email,mySybject,myContent)
-                send_mail(service,'me',message)
-        return 1
+                if(i==0):
+                    emails+=email
+                    i = 1
+                else:
+                    emails += ',' + email
+        if(i==1):
+            #message = create_mail('',emails,mySybject,myContent)
+            message = create_message_with_attachment('',emails,mySybject,myContent)
+            send_mail(service,'me',message)
+            return 1
     return 0

c7b218517716b0e3fc92c8bf492db519414e06d0
sidou <sidahmed.issolah@gmail.com>
2020-04-03 19:34:38 +0200

    creation of a class gmail that contains gmail's functions and delet the other functions

--- automail-1.3/automail-1.3.py
+++ automail-1.3/automail-1.3.py
@@ -3,11 +3,13 @@ from send import send_all_mails
 import fileupload
 from IPython.core.display import display
 from tkinter import filedialog, Tk
+from Gmail import Gmail
 
+gmail_connection = Gmail()
 
 @eel.expose # Expose this function to Javascript
 def sendMailPy(myLink,mySybject,myContent):
-    return send_all_mails(myLink,mySybject,myContent)
+    return gmail_connection.send_all_mails(myLink,mySybject,myContent)
 
 @eel.expose
 def fileDialog():
--- automail-1.3/send_email.py
+++
@@ -1,9 +0,0 @@
-from googleapiclient import errors
-
-def send_mail(service, user_id, message):
-    try:
-        message = (service.users().messages().send(userId=user_id, body=message)
-                .execute())
-        return 'sent'
-    except errors.HttpError as error:
-        print ('An error occurred: %s' % error)
